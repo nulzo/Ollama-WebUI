40bebf78dfe215aa260535c2c67c90b09bba4ecc
nulzo <nolanpgregory@gmail.com>
2024-12-18 19:17:31 -0600

    [add] - changes

--- backend/api/models/auth/user.py
+++ backend/api/models/auth/user.py
@@ -11,3 +11,8 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        db_table = "user"
